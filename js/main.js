6371cc4cc573d601c990f51376d6ba37c7218a20
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-03-27 11:51:26 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandom(number) {
     let result = Math.floor(Math.random()*number);
-    console.log (result);
+    return console.log (result);
   }
 
 function checkMaxLength (line, maxlegth) {

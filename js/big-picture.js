a83ced8118556c48f2938ff5a8973f52c9184fcc
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-06 15:50:11 +0500

    Update big-picture.js

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const bigPicture = document.querySelector('.big-picture');
 const escapePressed = (ev) => ev.key === 'Escape' && closeBigPicture();
 document.addEventListener('keydown',(KeyboardEvent) => escapePressed(ev));
@@ -75,3 +76,101 @@ function closeBigPicture() {
     bigPicture.classList.add('hidden');
     document.body.classList.remove('modal-open');
 }
+=======
+import { AVATAR_IMAGE_SIZE, DEFAULT_RENDERED_COMMENTS, STEP_ADDED_COMMENTS } from "./constants";
+
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+const bigPictureCLoseBtn = bigPicture.querySelector('.big-picture__cancel');
+const bigPictureCommentsList = bigPicture.querySelector('.social__comments');
+const bigPictureCommentsLoaderBtn = bigPicture.querySelector('.comments-loader');
+const bigPictureCommentsCounter = bigPicture.querySelector('.social__comment-count');
+const bigPictureCaption = bigPicture.querySelector('.social__caption');
+const bigPictureLike = bigPicture.querySelector('.likes-count');
+
+let actualComments = [];
+let countRenderedComments = DEFAULT_RENDERED_COMMENTS;
+
+
+const getCommentTemplate = ({avatar, message, name}) => '<li class="social__comment">
+<img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
+<p class="social__text">${message}</p>
+</li>'
+
+
+const getCounterCommentsTemplate = (commentsCount) => '${Math.min(countRenderedComments, commentsCount)} из <span class="comments-count">${commentsCount}</span>'
+
+const getCounterComments = () => {
+    bigPictureCommentsCounter.innerHTML = '';
+    bigPictureCommentsCounter.insertAdjacentHTML('afterbegin', getCounterCommentsTemplate(actualComments.length));
+};
+
+const renderComments = () => {
+    getCounterComments();
+
+    bigPictureCommentsList.innerHTML = '';
+    const commentsTemplate = actualComments.slice(0, countRenderedComments).map((comment) => getCommentTemplate(comment)).join('');
+    bigPictureCommentsList.insertAdjacentHTML('afterbegin', commentsTemplate);
+
+    if (countRenderedComments >= actualComments.length) {
+        bigPictureCommentsLoaderBtn.removeEventListener('click', onBigPictureCommentsLoaderBtnClick);
+        bigPictureCommentsLoaderBtn.classList.add('hidden');
+    }
+}
+
+const initComments = ({comments}) => {
+    actualComments = comments.slice();
+    bigPictureCommentsList.innerHTML = '';
+
+    if (comments.length === 0) {
+        bigPictureCommentsLoaderBtn.classList.add('hidden');
+        bigPictureCommentsCounter.textContent = 'Нет комментариев';
+        return;
+    } 
+
+    renderComments();
+    bigPictureCommentsLoaderBtn.addEventListener('click', onBigPictureCommentsLoaderBtnClick);
+};
+
+const closeBigPicture = () => {
+    document.body.classList.contains('modal-open');
+    bigPicture.classList.add('hidden');
+    bigPictureCLoseBtn.removeEventListener('click', onBigPictureCloseBtnClick);
+    window.removeEventListener('keydown', onWindowEscKeydown);
+    bigPictureCommentsLoaderBtn.classList.remove('hidden');
+    bigPictureCommentsLoaderBtn.removeEventListener('click', onBigPictureCommentsLoaderBtnClick);
+    countRenderedComments = DEFAULT_RENDERED_COMMENTS;
+};
+
+function onBigPictureCloseBtnClick() {
+    closeBigPicture();
+};
+
+function onWindowEscKeydown(evt) {
+    if (evt.key === 'Escape') {
+        closeBigPicture();
+        document.removeEventListener('keydown', onWindowEscKeydown);
+    }
+}
+
+function onBigPictureCommentsLoaderBtnClick() {
+    countRenderedComments += STEP_ADDED_COMMENTS;
+    renderComments();
+}
+
+const showBigPicture = (photo) => {
+   document.body.classList.add('modal-open');
+   bigPicture.classList.remove('hidden');
+   bigPictureImg.setAttribute('src', photo.url);
+   bigPictureCaption.textContent = photo.description;
+   bigPictureLike.textContent = photo.likes;
+
+   initComments(photo);
+
+   bigPictureCLoseBtn.addEventListener('click', onBigPictureCloseBtnClick);
+   window.addEventListener('keydown', onWindowEscKeydown);
+};
+
+export { showBigPicture };
+
+>>>>>>> Stashed changes

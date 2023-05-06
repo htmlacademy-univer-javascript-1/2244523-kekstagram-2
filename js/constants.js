1117e18beb846ee9e2b8f82f39f06e9d07eb72c7
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-06 15:50:22 +0500

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,36 @@
+<<<<<<< Updated upstream
 
 
 
+=======
+export const DEFAULT_RENDERED_COMMENTS = 5;
+export const STEP_ADDED_COMMENTS = 5;
+export const MAX_COMMENT_LENGTH = 140;
+export const ALERT_SHOW_TIME = 3000;
+export const MAX_COUNT_RANDOM_PHOTO = 10;
+export const AVATAR_IMAGE_SIZE = 35;
+
+export const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+export const MaxHashtag = {
+    LENGTH: 20,
+    COUNT: 5
+};
+
+export const Scale = {
+    MAX: 100,
+    MIN: 25,
+    STEP: 25
+}
+
+export const PhotoValide = {
+    SUCCESS: 'success',
+    ERROR: 'error'
+}
+
+export const Filter = {
+    DEFAULT: 'filter-default',
+    RANDOM: 'filter-random',
+    DISCUSSED: 'filter-discussed'
+}
+>>>>>>> Stashed changes

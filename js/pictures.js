c6cc818dc0de3abd7495dabfb1eb5c0bb5cca38e
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-07 17:31:18 +0500

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,18 +1,94 @@
 
+import { getData } from './api.js';
+import { getBigPopup } from './big-picture.js';
+import { showCustomErrorMessage } from './error.js';
+import { debounce } from './util.js';
+import { showSortBlock, sortDefaultClick, sortRandomClick, sortDiscussedClick, sortInput, comparePicturesIds, comparePicturesComments } from './filters.js';
+import { RERENDER_DELAY, MAX_COUNT_RANDOM_PHOTO } from './constants.js'
+
+const picturesWrap = document.querySelector('.pictures');
+const usersPhotoListFragment = document.createDocumentFragment();
 
 
-const pictureTemplate = document.querySelector('#picture');
-const pictureContainer = document.querySelector('.pictures');
-
-export function insertPhotoMiniature (photoDescriptions) {
-    const pictureFragment = document.createDocumentFragment();
-    for (const {url, likes,comments} of photoDescriptions) {
-        const picture = pictureTemplate.cloneNode(true).content;
-        picture.querySelector('picture__img').src = url;
-        picture.querySelector('picture__likes').src = likes;
-        picture.querySelector('picture__comments').src = comments;
-        pictureFragment.appendChild(picture);
+const resetArray = () => {
+  Array.from(picturesWrap.children).forEach((item) => {
+    if (item.classList.contains('picture')) {
+      item.remove();
     }
+  });
+};
 
-    pictureContainer.appendChild(pictureFragment);
-}
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+
+const thumbnailsRandomRender = (userPhotos) => {
+  userPhotos
+    .slice()
+    .sort(() => Math.random() - 0.5)
+    .slice(0, MAX_COUNT_RANDOM_PHOTO)
+    .forEach(({ url, likes, comments }) => {
+      const photoElement = pictureTemplate.cloneNode(true);
+      photoElement.querySelector('.picture__img').src = url;
+      photoElement.querySelector('.picture__likes').textContent = likes;
+      photoElement.querySelector('.picture__comments').textContent = comments.length;
+      usersPhotoListFragment.appendChild(photoElement);
+    });
+  resetArray();
+  picturesWrap.appendChild(usersPhotoListFragment);
+  getBigPopup(userPhotos);
+};
+
+
+const thumbnailsRender = (userPhotos) => {
+  userPhotos.slice();
+
+  if (sortInput.value === 'default') {
+    userPhotos.sort(comparePicturesIds);
+  } else if (sortInput.value === 'random') {
+    thumbnailsRandomRender();
+  } else if (sortInput.value === 'discussed') {
+    userPhotos.sort(comparePicturesComments);
+  }
+
+  userPhotos.forEach(({ url, likes, comments }) => {
+    const photoElement = pictureTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    usersPhotoListFragment.appendChild(photoElement);
+  });
+
+  resetArray();
+  picturesWrap.appendChild(usersPhotoListFragment);
+  getBigPopup(userPhotos);
+};
+
+
+getData(
+  (photos) => {
+    thumbnailsRender(photos);
+    showSortBlock();
+
+    sortDefaultClick(debounce(
+      () => thumbnailsRender(photos),
+      RERENDER_DELAY,
+    ));
+
+    sortRandomClick(debounce(
+      () => thumbnailsRandomRender(photos),
+      RERENDER_DELAY,
+    ));
+
+    sortDiscussedClick(debounce(
+      () => thumbnailsRender(photos),
+      RERENDER_DELAY,
+    ));
+
+  },
+  () => showCustomErrorMessage('Что-то пошло не так. Попробуйте перезагрузить страницу'),
+);
+
+export { thumbnailsRender, picturesWrap };

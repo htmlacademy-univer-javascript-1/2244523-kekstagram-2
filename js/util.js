f2885a94b9e63c4b510e5b98b3c8870babae66ee
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-07 17:31:26 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,10 +1,24 @@
-const getRandomPositiveInteger = (a, b) => {
+export const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
 
-  const getRandomArrayElement = (elements) => {
+export const getRandomArrayElement = (elements) => {
   return elements[getRandomPositiveInteger(0, elements.length - 1)];
 };
+
+const isEscape = (evt) => evt.key === 'Escape';
+
+export { isEscape };
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {debounce};

2ed58f658d0a1b9b0779456a6e2a763f1430f7d7
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-10 00:00:52 +0500

    Update user-form.js

--- js/user-form.js
+++ js/user-form.js
@@ -1,90 +1,50 @@
-import './../nouislider/nouislider.js';
-
-import { sliderElementBlock, sliderElement, effectLevelValue, imgUploadPreview } from './slider.js';
 import { isEscape } from './util.js';
-import { sendData } from './api.js';
-import { fileChooser } from './new-photo.js';
-import { showErrorMessage, showSuccessMessage } from './error.js';
-import { hashtagValidate, commentValidate, hashtags, commentField, imageLoad, onHashtagsTextInput, commentTextInput } from './validate.js';
-import { scaleControlSmallerButton, scaleControlBiggerButton, scaleControlValue, scaleValueHidden, onScaleSmallerClick, onScaleBiggerClick } from './scale.js';
+import { onFormInput, refreshPrinstine } from './validate.js';
+import { reloadScale } from './scale.js';
+import { setDefaultEffects } from './slider.js';
 
-const formUploadImage = document.querySelector('.img-upload__form');
-const modalView = document.querySelector('body');
-const buttonModalClose = document.querySelector('.img-upload__cancel');
+const imgUploadFileInput=document.querySelector('.img-upload__input');
+const form=document.querySelector('.img-upload__form');
+const overlay=document.querySelector('.img-upload__overlay');
+const closingButton = document.querySelector('#upload-cancel');
 
+const isNoFocus = (evt) => !evt.target.classList.contains('text__hashtags')
+    && !evt.target.classList.contains('text__description') ;
 
+const closeForm = () => {
+  overlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
 
-const scaleChange = () => {
-  scaleControlSmallerButton.addEventListener('click', onScaleSmallerClick);
-  scaleControlBiggerButton.addEventListener('click', onScaleBiggerClick);
+  imgUploadFileInput.value='';
+
+  refreshPrinstine();
+  form.reset();
+  form.removeEventListener('submit', onFormInput);
 };
-
-
-formUploadImage.addEventListener('change', () => {
-  openFormPopup();
-});
-
-
-function openFormPopup() {
-  imageLoad.classList.remove('hidden');
-  modalView.classList.add('modal-open');
-  scaleChange();
-  hashtagValidate();
-  commentValidate();
-}
-
-
-buttonModalClose.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  closeFormPopup();
-});
-
-
-function closeFormPopup() {
-  modalView.classList.remove('modal-open');
-  imageLoad.classList.add('hidden');
-  
-  scaleControlSmallerButton.removeEventListener('click', onScaleSmallerClick);
-  scaleControlBiggerButton.removeEventListener('click', onScaleBiggerClick);
-  hashtags.removeEventListener('input', onHashtagsTextInput);
-  commentField.addEventListener('input', commentTextInput);
-  
-  fileChooser.value = '';
-  scaleValueHidden.value = '100';
-  scaleControlValue.value = '100%';
-  imgUploadPreview.style.transform = 'scale(1)';
-  
-  sliderElementBlock.style.display = 'none';
-  sliderElement.style.display = 'none';
-  effectLevelValue.value = '';
-  imgUploadPreview.style.filter = 'none';
-  
-  hashtags.value = '';
-  commentField.value = '';
-}
-
-
-window.addEventListener('keydown', (evt) => {
-  if (isEscape(evt)) {
-    evt.preventDefault();
-    closeFormPopup();
+const onClosingButtonClick =() => {
+  closeForm();
+  closingButton.removeEventListener('click', onClosingButtonClick);
+};
+const onEscKeyDown = (evt) => {
+  if (isEscape(evt) && isNoFocus(evt)){
+    closeForm();
+    closingButton.removeEventListener('click', onClosingButtonClick);
+    document.removeEventListener('keydown', onEscKeyDown);
   }
-  window.removeEventListener('keydown', closeFormPopup);
-});
-
-
-const setImgUploadFormSubmit = (onSuccess) => {
-  formUploadImage.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    sendData(
-      () => showSuccessMessage('Форма успешно отправлена'),
-      () => showErrorMessage('При отправке формы возникла ошибка'),
-      () => onSuccess(),
-      new FormData(evt.target),
-    );
-  });
 };
 
-setImgUploadFormSubmit(closeFormPopup);
+const onFileInput = () => {
+  reloadScale();
+  setDefaultEffects();
 
-export { formUploadImage, imageLoad, imgUploadPreview };
+  overlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  closingButton.addEventListener('click', onClosingButtonClick);
+  document.addEventListener('keydown', onEscKeyDown);
+  form.addEventListener('submit', onFormInput);
+};
+
+imgUploadFileInput.addEventListener('input', onFileInput);
+
+export{onEscKeyDown, closeForm};

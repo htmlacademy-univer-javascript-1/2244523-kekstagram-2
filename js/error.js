d51e97dfd1942cc939944917aa2f2c9cfbb3de5a
AngelinaArtemeva <angelinakisa88@gmail.com>
2023-05-09 15:39:32 +0500

    Update error.js

--- js/error.js
+++ js/error.js
@@ -1,5 +1,6 @@
 import { isEscape } from './util.js';
 
+const ALERT_SHOW_TIME = 5000;
 
 const errorContainerTemplate = document.querySelector('#error')
   .content
@@ -82,6 +83,40 @@ const showSuccessMessage = () => {
   successCloseButton.addEventListener('click', onCloseMessageClick);
   document.addEventListener('keydown', onPopupEscKeydown);
   document.addEventListener('click', onOutBoxClick);
+
+  setTimeout(() => {
+    successContainer.remove();
+  }, ALERT_SHOW_TIME);
 };
 
-export { showErrorMessage, showSuccessMessage };
+const showCustomErrorMessage = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '50%';
+  alertContainer.style.top = '40%';
+  alertContainer.style.bottom = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.transform = 'translate(-50%, -50%)';
+  alertContainer.style.height = '30%';
+  alertContainer.style.borderRadius = '10px';
+  alertContainer.style.display = 'flex';
+  alertContainer.style.justifyContent = 'center';
+  alertContainer.style.alignItems = 'center';
+  alertContainer.style.padding = '50px 50px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.lineHeight = '1.2';
+  alertContainer.style.backgroundColor = '#FF5F49';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+
+export { showErrorMessage, showSuccessMessage, showCustomErrorMessage };
